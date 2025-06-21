586cbbba1ea4007504f6c3a1a3fc8f0e7dd68cdd
mahfuzarrahmanmunna <mdmahfuzarrahmanmunna44@gmail.com>
2025-06-21 21:30:01 +0600

    added to login and sign in method.

--- src/Provider/AuthProvider/AuthProvider.jsx
+++ src/Provider/AuthProvider/AuthProvider.jsx
@@ -1,18 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { AuthContext } from '../../Context/AuthContext/AuthContext';
 import { auth } from '../../Firebase/firebase.init';
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword } from 'firebase/auth';
 
 const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true)
+
+    // Create User
     const createUser = (email, password) => {
+        setLoading(true)
         return createUserWithEmailAndPassword(auth, email, password)
     }
 
+    // Sign in with email and password
     const signIn = (email, password) => {
+        setLoading(true)
         return signInWithEmailAndPassword(auth, email, password)
     }
 
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser);
+            setLoading(false)
+        })
+        return () => {
+            unsubscribe()
+        }
+    }, [])
+
     const authInfo = {
+        user,
+        loading,
         createUser,
         signIn,
     }
