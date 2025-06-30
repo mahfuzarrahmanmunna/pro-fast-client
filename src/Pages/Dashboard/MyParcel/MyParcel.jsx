d385376a2b661625a514ecae42c6874240820daa
mahfuzarrahmanmunna <mdmahfuzarrahmanmunna44@gmail.com>
2025-07-01 00:24:41 +0600

    added tanstack query

--- src/Pages/Dashboard/MyParcel/MyParcel.jsx
+++ src/Pages/Dashboard/MyParcel/MyParcel.jsx
@@ -1,6 +1,21 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import useAuth from '../../../Hooks/useAuth/useAuth';
+import useAxiosSecure from '../../../Hooks/useAuth/useAxiosSecure/useAxiosSecure';
 
 const MyParcel = () => {
+    const { user } = useAuth()
+    const axiosSecure = useAxiosSecure()
+
+    const { data: parcels = [] } = useQuery({
+        queryKey: ['my-parcels', user?.email],
+        queryFn: async () => {
+            const res = await axiosSecure.length(`parcels?=email=${user?.email}`);
+            return res.data
+        }
+    })
+
+    console.log(parcels);
     return (
         <div>
             myParcel is coming here
--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,15 @@ import { router } from './Routes/Routes.jsx'
 import AOS from 'aos';
 import 'aos/dist/aos.css'; // You can also use <link> for styles
 import AuthProvider from './Provider/AuthProvider/AuthProvider.jsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 AOS.init()
-
+const queryClient = new QueryClient()
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AuthProvider>
-      <RouterProvider router={router} />
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
+    </QueryClientProvider>
   </StrictMode>,
 )
