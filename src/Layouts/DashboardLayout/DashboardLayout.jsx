15a746b5ee35f2f06dc63ff33ee2c3fc2a6be2a8
mahfuzarrahmanmunna <mdmahfuzarrahmanmunna44@gmail.com>
2025-06-30 13:51:45 +0600

    added and edit dashboard layout

--- src/Layouts/DashboardLayout/DashboardLayout.jsx
+++ src/Layouts/DashboardLayout/DashboardLayout.jsx
@@ -4,11 +4,28 @@ const DashboardLayout = () => {
     return (
         <div className="drawer lg:drawer-open">
             <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-            <div className="drawer-content flex flex-col items-center justify-center">
+            <div className="drawer-content flex flex-col ">
                 {/* Page content here */}
-                <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">
-                    Open drawer
-                </label>
+                <div className="navbar bg-base-300 w-full block lg:hidden">
+                    <div className="flex-none lg:hidden">
+                        <label htmlFor="my-drawer-2" aria-label="open sidebar" className="btn btn-square btn-ghost">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                className="inline-block h-6 w-6 stroke-current"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M4 6h16M4 12h16M4 18h16"
+                                ></path>
+                            </svg>
+                        </label>
+                    </div>
+                    <div className="mx-2 flex-1 px-2">Dashboard</div>
+                </div>
             </div>
             <div className="drawer-side">
                 <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay"></label>
